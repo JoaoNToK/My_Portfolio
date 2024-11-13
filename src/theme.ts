b0da2e665559f59e1073f124fa33bb3393d520f4
João Neto <joaonetoneto1331@gmail.com>
2024-11-13 14:22:56 -0300

    feat: configs material ui theme, css basiline

---
+++ src/theme.ts
@@ -0,0 +1,24 @@
+import { createTheme, responsiveFontSizes } from "@mui/material";
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: "#232323",
+    },
+    secondary: {
+      main: "#4f8e3e",
+    },
+  },
+  typography: {
+    fontFamily: "Segoe UI",
+    fontSize: 16,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+  },
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
